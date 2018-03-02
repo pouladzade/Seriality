659d1bbec002f5ac156c9fc5164551f7b7ddad93
pouladzade <pouladzade@gmail.com>
2018-03-02 19:05:35 +0800

    Adding the Seriality contract

---
+++ Seriality.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.16;
+
+import "./BytesToTypes.sol";
+import "./TypesToBytes.sol";
+import "./SizeOf.sol";
+
+contract Seriality is BytesToTypes, TypesToBytes {
+
+    function seriality() public {
+
+    }
+}
